8a6d0fc93783f7e46be79f9a34e139891307d055
esanche06 <ethands@uci.edu>
2017-03-07 12:13:31 -0800

    fixed hookshot touching goal creating instate

--- Assets/Scripts/detectWinner.cs
+++ Assets/Scripts/detectWinner.cs
@@ -7,18 +7,19 @@ public class detectWinner : MonoBehaviour {
     public GameObject GState; //GameState
 	public Text winText1;
 	public Text winText2;
+	public GameObject Player2;
 
 
 	void OnCollisionEnter(Collision col){
-        if (col.gameObject.name == "Player1" && !GState.GetComponent<GameState>().TimedOut && !GState.GetComponent<GameState>().BlueWinner)
-        {
-            GState.GetComponent<GameState>().RedWinner = true;
-        }
-        if (col.gameObject.name == "Player2" && !GState.GetComponent<GameState>().TimedOut && !GState.GetComponent<GameState>().RedWinner)
-        {
-            GState.GetComponent<GameState>().BlueWinner = true;
-        }
-        GState.GetComponent<GameState>().GameWon = true;
+		if (Vector3.Distance (gameObject.transform.position, col.gameObject.transform.position) <= 2) {
+			if (col.gameObject.name == "Player1" && !GState.GetComponent<GameState> ().TimedOut && !GState.GetComponent<GameState> ().BlueWinner) {
+				GState.GetComponent<GameState> ().RedWinner = true;
+			}
+			if (col.gameObject.name == "Player2" && !GState.GetComponent<GameState> ().TimedOut && !GState.GetComponent<GameState> ().RedWinner) {
+				GState.GetComponent<GameState> ().BlueWinner = true;
+			}
+			GState.GetComponent<GameState> ().GameWon = true;
+		}
 	}
 
 	// Use this for initialization
@@ -29,6 +30,6 @@ public class detectWinner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		print(Vector3.Distance(gameObject.transform.position, Player2.transform.position));
 	}
 }
